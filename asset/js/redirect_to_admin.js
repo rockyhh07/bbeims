7810eb931a4710e03d6050589fc4c9c8e7119fc1
FGL200 <ericlandicho20@gmail.com>
2024-02-03 20:13:30 +0800

    old

--- asset/js/redirect_to_admin.js
+++ asset/js/redirect_to_admin.js
@@ -1,6 +1,6 @@
-fetch(BASE_URL + '/php/session_status.php')
-.then(response=>response.json())
-.then(response=>{
-    session = response.result[0];
-    if(session.result) window.location.href = BASE_URL + 'admin/';
-});
+// fetch(BASE_URL + '/php/session_status.php')
+// .then(response=>response.json())
+// .then(response=>{
+//     session = response.result[0];
+//     if(session.result) window.location.href = BASE_URL + 'admin/';
+// });
